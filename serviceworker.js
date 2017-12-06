33edf97e3657e16d0ea3c4cbfb689d03962e3abc
Timothy B. Smith <smith@ttimsmith.com>
2017-12-05 22:26:35 -0600

    Update service worker to show most recent html

--- serviceworker.js
+++ serviceworker.js
@@ -12,6 +12,13 @@
 
   var staticCacheName = 'cache';
 
+  var addToCache = function(request, response) {
+    caches.open(staticCacheName)
+      .then(function (cache) {
+        cache.put(request, response);
+      });
+  }
+
   self.addEventListener('install', function (event) {
     console.log('Attempting to install service worker and cache static assets');
     event.waitUntil(
@@ -24,29 +31,56 @@
 
   self.addEventListener('fetch', function (event) {
     console.log('Fetch event for ', event.request.url);
-    event.respondWith(
-      caches.match(event.request).then(function (response) {
-        if (response) {
-          console.log('Found ', event.request.url, ' in cache');
+
+    var type = event.request.headers.get('Accept');
+    // network-first for HTML documents
+    if (type.includes('text/html')) {
+      event.respondWith(
+        // try fetching a resource from the network
+        fetch(event.request).then(response => {
+          // cache the resource and serve it
+          let responseCopy = response.clone();
+          addToCache(event.request.url, responseCopy); // this is a custom function, I'll elaborate on it later
           return response;
-        }
-        console.log('Network request for ', event.request.url);
-        return fetch(event.request).then(function (response) {
-          if (response.status === 404) {
-            return caches.match('404.html');
-          }
-          return caches.open(staticCacheName).then(function (cache) {
-            if (event.request.url.indexOf('test') < 0) {
-              cache.put(event.request.url, response.clone());
-            }
+        })
+        .catch(function(error) {
+          // the resource could not be fetched from network
+          // so let's pull it out from cache, otherwise serve the "offline" page
+          return caches.match(event.request)
+            .then(function (response) {
+              response || caches.match('/offline/');
+            })
+        })
+      );
+    }
+    // cache-first for assets
+    else {
+      event.respondWith(
+        caches.match(event.request).then(function (response) {
+          if (response) {
+            console.log('Found ', event.request.url, ' in cache');
             return response;
+          }
+          console.log('Network request for ', event.request.url);
+          return fetch(event.request).then(function (response) {
+            if (response.status === 404) {
+              return caches.match('404.html');
+            }
+            return caches.open(staticCacheName).then(function (cache) {
+              if (event.request.url.indexOf('test') < 0) {
+                cache.put(event.request.url, response.clone());
+              }
+              return response;
+            });
           });
-        });
-      }).catch(function (error) {
-        console.log('Error, ', error);
-        return caches.match('offline/index.html');
-      })
-    );
+        }).catch(function (error) {
+          console.log('Error, ', error);
+          return caches.match('offline/index.html');
+        })
+      );
+    }
+
+    
   });
 
   self.addEventListener('activate', function (event) {
