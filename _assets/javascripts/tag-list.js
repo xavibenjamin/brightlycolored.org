698ae47d20e4659dd7cc98898bdce6ac18d8d32f
Timothy B. Smith <smith@ttimsmith.com>
2017-12-16 00:37:29 -0600

    Check to see if element exists first

--- _assets/javascripts/tag-list.js
+++ _assets/javascripts/tag-list.js
@@ -4,14 +4,15 @@
 // a button is clicked.
 // -----------------------------------------
 
-// var tagList = document.querySelector('.tag-list');
-// var tagButton = document.getElementById('tag-list-toggle');
+var tagList = document.querySelector('.tag-list');
+var tagButton = document.getElementById('tag-list-toggle');
 
-// function tagListReveal() {
-//   tagList.classList.add('js-is-open');
-//   tagButton.remove();
-// }
+function tagListReveal() {
+  tagList.classList.add('js-is-open');
+  tagButton.remove();
+}
 
-// window.onload=function() {
-//   tagButton.addEventListener("click", tagListReveal);
-// }
+if ( tagButton ) {
+  tagButton.addEventListener("click", tagListReveal);
+}
+
