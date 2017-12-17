5b353f140379b197340a18d5b33997b895341320
Timothy B. Smith <smith@ttimsmith.com>
2017-12-16 21:54:17 -0600

    Remove code for search page

--- search.js
+++ search.js
@@ -60,34 +60,4 @@ jQuery(document).ready(function() {
   }
 
   $search_input.on('keyup', searchCallback);
-
-  // Set up some variables
-  var split;
-  var qObj = {};
-
-  // Get query string
-  var qs = window.location.search;
-  qs = qs.split('?')[1];
-
-  // If there is a query string
-  if (qs) {
-    // Split on the ampersand seperator
-    qs = qs.split('&');
-
-    // Iterate over number of items in qs
-    for (var i = 0; i < qs.length; i++) {
-      // Split the item
-      split = qs[i].split('=');
-
-      // Add the item to the qObj
-      qObj[split[0]] = split[1];
-    }
-    // Check for q param
-    if (qObj.q) {
-      // Set the value of the input
-      $('#search-query').val(decodeURIComponent(qObj.q)
-        .replace('+', ' '))
-        .trigger('keyup');
-    }
-  }
 });
