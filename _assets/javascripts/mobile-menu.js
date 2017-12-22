dd8e2affe1e41da1ea968cf924ff3e3f3c0ee1a3
Timothy B. Smith <smith@ttimsmith.com>
2017-12-21 23:33:57 -0600

    Make mobile nav and search nicer

--- _assets/javascripts/mobile-menu.js
+++ _assets/javascripts/mobile-menu.js
@@ -6,22 +6,10 @@
 
 var mobileMenuButton = document.getElementById('nav-icon');
 var mobileMenu = document.getElementById('sidebar');
-var mobileHeader = document.getElementById('header');
-var mobileContent = document.getElementById('main-content');
-
 
 function mobileMenuToggle() {
   mobileMenuButton.classList.toggle('js-menu-is-open');
   mobileMenu.classList.toggle('js-menu-is-open');
-  mobileHeader.classList.toggle('js-menu-is-open');
-
-  if ( mobileHeader.classList.contains('js-menu-is-open') ) {
-    mobileContent.style.paddingTop = 'calc(4rem + 51.8px)';
-  } else {
-    mobileContent.style.paddingTop = null;
-  }
 }
-
-
 
 mobileMenuButton.addEventListener("click", mobileMenuToggle);
