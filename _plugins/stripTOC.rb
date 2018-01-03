f391ceefe1032ca54d74fe6abf95edacca032d6a
Tim Smith <smith@ttimsmith.com>
2018-01-03 15:28:52 -0600

    BC-79 Remove toc from feed (#80)
    - Closes #79

--- _plugins/stripTOC.rb
+++ _plugins/stripTOC.rb
@@ -6,9 +6,9 @@ module Jekyll
     def strip_toc(raw)
       doc = Nokogiri::HTML.fragment(raw.encode('UTF-8', :invalid => :replace, :undef => :replace, :replace => ''))
 
-      for block in ['ul'] do
+      for block in ['div'] do
         doc.css(block).each do |ele|
-          ele.remove if (ele['id'] == 'markdown-toc')
+          ele.remove if (ele['id'] == 'post-table-of-contents')
         end
       end
 
