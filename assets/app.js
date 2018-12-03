42d3ce4594c9abada806d390bed0004edc2e0a8b
Timothy B. Smith <smith@ttimsmith.com>
2018-12-02 19:23:56 -0600

    Remove newsletter javascript and jQuery

--- assets/app.js
+++ assets/app.js
@@ -2,7 +2,6 @@
 ---
 
 {% capture scripts %}
-  {% include javascripts/newsletter.js %}
   {% include javascripts/heading-anchors.js %}
   {% include javascripts/group-list.js %}
   {% include javascripts/social-links.js %}
