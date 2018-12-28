e7aa90a4067161078389875755802eb528f93864
Timothy B. Smith <smith@ttimsmith.com>
2018-12-28 15:37:15 -0600

    delete social links js

--- assets/app.js
+++ assets/app.js
@@ -4,7 +4,6 @@
 {% capture scripts %}
   {% include javascripts/heading-anchors.js %}
   {% include javascripts/group-list.js %}
-  {% include javascripts/social-links.js %}
   {% include javascripts/toc.js %}
 {% endcapture %}
 {{ scripts | strip }}
